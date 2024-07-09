a3fd2abdf70fc3df96cd8a1d007f719a95fb7879
manzitresor <manzitre@gmail.com>
2024-07-09 22:25:36 +0200

    Add challenges

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,94 @@
 "use strict";
-// data Types
-let id = 5;
-let myName = 'Manzi';
-let age = 32;
-// Arrays
-let ids = [1, 2, 3, 4];
-let arr = [1, 'manzi', true];
-//  Tuples
-let person = [1, 'manzi', true];
-let employee;
-employee = [
-    [1, 'Ramona'],
-    [2, 'Albert']
-];
-// Union
-let unionID;
-unionID = 2;
-// enums
-var direction;
-(function (direction) {
-    direction[direction["up"] = 0] = "up";
-    direction[direction["down"] = 1] = "down";
-    direction[direction["left"] = 2] = "left";
-    direction[direction["right"] = 3] = "right";
-})(direction || (direction = {}));
-const obj = {
-    id: 1,
-    name: 'manzi'
-};
-// Type assertion
-let cid;
-// let customId = <number>cid;
-let customId = cid;
-customId = 12;
-// Functions
-function addNumber(num1, num2) {
-    return num1 + num2;
-}
-console.log(addNumber(4, 5));
-function log(message) {
-    console.log(message);
-}
-log('Hello world!');
-const user1 = {
-    id: 1,
-    name: 'manzi'
-};
-let InterfaceNum = (x, y) => x + y;
-class Person {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-    getName() {
-        return `Hello ${this.name}`;
-    }
-}
-const person1 = new Person(1, 'Manzi');
-console.log(person1);
+// // data Types
+// let id: number = 5;
+// let myName: string = 'Manzi';
+// let age: any = 32
+// // Arrays
+//  let ids: number[] = [1,2,3,4];
+//  let arr: any[] = [1,'manzi',true];
+// //  Tuples
+// let person: [number,string,boolean] = [1,'manzi',true];
+// let employee: [number,string][]
+// employee= [
+//     [1,'Ramona'],
+//     [2,'Albert']
+// ]
+// // Union
+// let unionID: string | number
+// unionID = 2 
+// // enums
+// enum direction {
+//     up,
+//     down,
+//     left,
+//     right
+// }
+// // Objects
+// type User = {
+//     id: number,
+//     name: string
+// }
+// const obj:User = {
+//     id: 1,
+//     name:'manzi'
+// }
+// // Type assertion
+// let cid: any
+// // let customId = <number>cid;
+// let customId = cid as number;
+// customId = 12;
+// // Functions
+// function addNumber(num1:number,num2:number):number {
+//     return num1 + num2;
+// }
+// console.log(addNumber(4,5))
+// function log(message: string | number) :void{
+//     console.log(message)
+// }
+// log('Hello world!')
+// // Interfaces
+// // 1. INTERFACE WITH OBJECTS
+// interface userInterface {
+//     id: number,
+//     name: string
+// }
+// const user1: userInterface = {
+//     id: 1,
+//     name: 'manzi'
+// }
+// // 2. INTERFACE WITH FUNCTIONS
+// interface MathFunc {
+//     (x: number, y: number): number
+// }
+// let InterfaceNum:MathFunc = (x:number,y:number):number => x + y;
+// // Classes
+// // class Person {
+// //     id: number;
+// //     name: string;
+// //     constructor(id:number,name:string) {
+// //         this.id = id;
+// //         this.name = name;
+// //     }
+// //     getName() {
+// //         return this.name;
+// //     }
+// // }
+// // Interface with classes
+// interface personType {
+//     id: number,
+//     name: string,
+//     getName(): string
+// }
+// class Person implements personType {
+//     id: number;
+//     name: string;
+//     constructor(id:number,name:string) {
+//         this.id = id;
+//         this.name = name;
+//     }
+//     getName() {
+//         return `Hello ${this.name}`;
+//     }
+// }
+// const person1 = new Person(1,'Manzi')
+// console.log(person1)
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,111 @@
 
-// data Types
-let id: number = 5;
-let myName: string = 'Manzi';
-let age: any = 32
+// // data Types
+// let id: number = 5;
+// let myName: string = 'Manzi';
+// let age: any = 32
 
-// Arrays
- let ids: number[] = [1,2,3,4];
- let arr: any[] = [1,'manzi',true];
+// // Arrays
+//  let ids: number[] = [1,2,3,4];
+//  let arr: any[] = [1,'manzi',true];
 
-//  Tuples
+// //  Tuples
 
-let person: [number,string,boolean] = [1,'manzi',true];
-let employee: [number,string][]
-employee= [
-    [1,'Ramona'],
-    [2,'Albert']
-]
+// let person: [number,string,boolean] = [1,'manzi',true];
+// let employee: [number,string][]
+// employee= [
+//     [1,'Ramona'],
+//     [2,'Albert']
+// ]
 
-// Union
-let unionID: string | number
-unionID = 2 
+// // Union
+// let unionID: string | number
+// unionID = 2 
 
-// enums
+// // enums
 
-enum direction {
-    up,
-    down,
-    left,
-    right
-}
+// enum direction {
+//     up,
+//     down,
+//     left,
+//     right
+// }
 
-// Objects
+// // Objects
 
-type User = {
-    id: number,
-    name: string
-}
-const obj:User = {
-    id: 1,
-    name:'manzi'
-}
+// type User = {
+//     id: number,
+//     name: string
+// }
+// const obj:User = {
+//     id: 1,
+//     name:'manzi'
+// }
 
-// Type assertion
+// // Type assertion
 
-let cid: any
-// let customId = <number>cid;
-let customId = cid as number;
-customId = 12;
+// let cid: any
+// // let customId = <number>cid;
+// let customId = cid as number;
+// customId = 12;
 
-// Functions
+// // Functions
 
-function addNumber(num1:number,num2:number):number {
-    return num1 + num2;
-}
+// function addNumber(num1:number,num2:number):number {
+//     return num1 + num2;
+// }
 
-console.log(addNumber(4,5))
+// console.log(addNumber(4,5))
 
-function log(message: string | number) :void{
-    console.log(message)
-}
+// function log(message: string | number) :void{
+//     console.log(message)
+// }
 
-log('Hello world!')
+// log('Hello world!')
 
-// Interfaces
+// // Interfaces
 
-// 1. INTERFACE WITH OBJECTS
-interface userInterface {
-    id: number,
-    name: string
-}
+// // 1. INTERFACE WITH OBJECTS
+// interface userInterface {
+//     id: number,
+//     name: string
+// }
 
-const user1: userInterface = {
-    id: 1,
-    name: 'manzi'
-}
+// const user1: userInterface = {
+//     id: 1,
+//     name: 'manzi'
+// }
 
-// 2. INTERFACE WITH FUNCTIONS
+// // 2. INTERFACE WITH FUNCTIONS
 
-interface MathFunc {
-    (x: number, y: number): number
-}
+// interface MathFunc {
+//     (x: number, y: number): number
+// }
 
-let InterfaceNum:MathFunc = (x:number,y:number):number => x + y;
+// let InterfaceNum:MathFunc = (x:number,y:number):number => x + y;
 
-// Classes
+// // Classes
 
-// class Person {
+// // class Person {
+// //     id: number;
+// //     name: string;
+// //     constructor(id:number,name:string) {
+// //         this.id = id;
+// //         this.name = name;
+// //     }
+// //     getName() {
+// //         return this.name;
+// //     }
+// // }
+
+
+// // Interface with classes
+// interface personType {
+//     id: number,
+//     name: string,
+//     getName(): string
+// }
+
+// class Person implements personType {
 //     id: number;
 //     name: string;
 //     constructor(id:number,name:string) {
@@ -93,29 +113,9 @@ let InterfaceNum:MathFunc = (x:number,y:number):number => x + y;
 //         this.name = name;
 //     }
 //     getName() {
-//         return this.name;
+//         return `Hello ${this.name}`;
 //     }
 // }
 
-
-// Interface with classes
-interface personType {
-    id: number,
-    name: string,
-    getName(): string
-}
-
-class Person implements personType {
-    id: number;
-    name: string;
-    constructor(id:number,name:string) {
-        this.id = id;
-        this.name = name;
-    }
-    getName() {
-        return `Hello ${this.name}`;
-    }
-}
-
-const person1 = new Person(1,'Manzi')
-console.log(person1)
+// const person1 = new Person(1,'Manzi')
+// console.log(person1)
