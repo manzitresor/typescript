aa339a04be4b57c97cc4df521bcb09f13493adaa
manzitresor <manzitre@gmail.com>
2024-09-02 22:53:36 +0200

    add find unique number challenge

---
+++ src/challenges/Find_the_unique_number.ts
@@ -0,0 +1,26 @@
+/*
+
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique
+FundamentalsAlgorithms
+
+*/
+
+function findUniq(arr:number[]):number {
+    const sorted: number [] = arr.sort((a:number,b:number) => a - b)
+   if(sorted[0] !== sorted[1]) {
+     return sorted[0]
+     }
+  return sorted[ sorted.length - 1]
+ }
