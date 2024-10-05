2e2764f1e186a368c2dc04b7bf0aa977deaf521e
manzitresor <manzitre@gmail.com>
2024-10-05 09:40:00 +0200

    Add countDivisors

---
+++ src/challenges/CountDivisors.ts
@@ -0,0 +1,28 @@
+/* 
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000, but fixed tests go higher.
+
+EXAMPLES:
+
+4 --> 3 // we have 3 divisors - 1, 2 and 4
+5 --> 2 // we have 2 divisors - 1 and 5
+12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+*/
+function getDivisorsCnt(n: number): number {
+    let count = 0;
+    
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        if (i === n / i) {
+          count++;
+        } else {
+          count += 2;
+        }
+      }
+    }
+    
+    return count;
+  }
+  
